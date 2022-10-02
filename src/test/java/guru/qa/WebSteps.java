b84530ae239280d61412831165e19c4fb410dbde
noredcat <anastasiaredcat@gmail.com>
2022-10-02 14:59:45 +0700

    delete not using imports

--- src/test/java/guru/qa/WebSteps.java
+++ src/test/java/guru/qa/WebSteps.java
@@ -1,11 +1,8 @@
 package guru.qa;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.WebDriverRunner;
-import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
+
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
